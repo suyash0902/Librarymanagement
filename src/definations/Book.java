c4fb3ef07db660661b04eb59eb1e3346b68de9a6
Suyash <suyash.sri,0902@gmail.com>
2020-08-29 18:49:17 +0530

    create array named availableLibraryBooks in Library.java

--- src/definations/Book.java
+++ src/definations/Book.java
@@ -15,30 +15,35 @@ public class Book {
     private String authorName;
     private String isbnNumber;
 
-    //Setter Method & Getter Method()
-    public String getBookName() {
-        return bookName;
-    }
-    public void setBookName(String bookName) {
-        this.bookName = bookName;
-    }
-    public String getAuthorName() {
-        return authorName;
-    }
-    public void setAuthorName(String authorName) {
-        this.authorName = authorName;
-    }
-    public String getIsbnNumber() {
-        return isbnNumber;
-    }
-    public void setIsbnNumber(String isbnNumber) {
-        this.isbnNumber = isbnNumber;
-    }
-
     //Parameterized constructor()
     public Book(String bookName, String authorName, String isbnNumber) {
         this.bookName = bookName;
         this.authorName = authorName;
+        this.isbnNumber = isbnNumber;
+    }
+
+    //Setter Method & Getter Method()
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getIsbnNumber() {
+        return isbnNumber;
+    }
+
+    public void setIsbnNumber(String isbnNumber) {
         this.isbnNumber = isbnNumber;
     }
 
---
+++ src/definations/Library.java
@@ -0,0 +1,14 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Suyash Srivastava (suyash0902)
+ *   Date: 29-08-2020
+ *   Time: 18:46
+ *   File: Library.java
+ */
+
+package definations;
+
+public class Library {
+    private Book[] availableLibraryBooks;
+
+}
+
