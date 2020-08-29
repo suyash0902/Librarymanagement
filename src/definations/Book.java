e7ee82cdb6581f18b6c33aeb22e67610b71391e4
Suyash <suyash.sri,0902@gmail.com>
2020-08-29 18:31:59 +0530

    create member variables in Book.java

---
+++ src/definations/Book.java
@@ -0,0 +1,17 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Suyash Srivastava (suyash0902)
+ *   Date: 29-08-2020
+ *   Time: 18:30
+ *   File: Book.java
+ */
+
+package definations;
+
+public class Book {
+    // Creating member variable
+    private String bookName;
+    private String authorName;
+    private String isbnNumber;
+
+}
+
