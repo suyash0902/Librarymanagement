3e747993c389b03e45eac7be1fc6cba400c7e50e
Suyash <suyash.sri,0902@gmail.com>
2020-08-29 18:39:27 +0530

    create parameterized constructor in Book.java

--- src/definations/Book.java
+++ src/definations/Book.java
@@ -12,29 +12,33 @@ public class Book {
     private String bookName;
     private String authorName;
     private String isbnNumber;
+
     //Setter Method & Getter Method
     public String getBookName() {
         return bookName;
     }
-
     public void setBookName(String bookName) {
         this.bookName = bookName;
     }
-
     public String getAuthorName() {
         return authorName;
     }
-
     public void setAuthorName(String authorName) {
         this.authorName = authorName;
     }
-
     public String getIsbnNumber() {
         return isbnNumber;
     }
-
     public void setIsbnNumber(String isbnNumber) {
         this.isbnNumber = isbnNumber;
     }
+
+    //parameterized constructor
+    public Book(String bookName, String authorName, String isbnNumber) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.isbnNumber = isbnNumber;
+    }
+
 }
 
